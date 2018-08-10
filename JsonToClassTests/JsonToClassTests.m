f3ed2a8ea3450f5a9bb70a66deeaf5db7691777f
박길호 <p988030@naver.com>
2018-08-10 23:35:45 +0900

    no message

---
+++ JsonToClassTests/JsonToClassTests.m
@@ -0,0 +1,34 @@
+//
+//  JsonToClassTests.m
+//  JsonToClassTests
+//
+//  Created by pkh on 2014. 8. 26..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JsonToClassTests : XCTestCase
+
+@end
+
+@implementation JsonToClassTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
