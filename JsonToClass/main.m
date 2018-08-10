6f3316b0e1d106ac9fb4d0c65356213af1896016
박길호 <p988030@naver.com>
2018-08-10 23:09:23 +0900

    Add files via upload

---
+++ JsonToClass/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  JsonToClass
+//
+//  Created by pkh on 2014. 8. 26..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
